0b11d4912af30705ac57a8bf67b631d4dbc349be
haneen <haneenjaradeh1@gmail.com>
2021-09-22 18:52:30 +0300

    lab07

--- server.js
+++ server.js
@@ -1,9 +1,13 @@
 'use strict';
 
 const express = require('express');
+require('dotenv').config();
+const cors = require('cors');
 const server = express();
-const PORT = 3000;
-const weatherData = require('./assets/weather.json')
+const PORT = process.env.PORT;
+server.use(cors());
+const getWeather = require('./assets/weather.json')
+
 
 server.get('/', (req, res) => {
 
@@ -12,38 +16,51 @@ server.get('/', (req, res) => {
 server.get('/test', (request, response) => {
   response.send('api server is working');
 });
-server.get('*', (request, response) => {
-    response.send('route is not found');
-  });
 
-let array=[];
 server.get('/getWeather', (req, res) => {
-res.send(weatherData)
-let DataOfWeather=req.query.DataOfWeather;
+// res.send(weatherData)
+let searchQuery= req.query.searchQuery;
 console.log(req.query);
-console.log(req.query.DataOfWeather)
-let weatherInfo = DataOfWeather.weather.find((item)=>{
-array.push(
-new Forecast(`${item.weatherData.description}, ${item.weatherData.date}`)
+// console.log(req.query.DataOfWeather)
+let weatherData = getWeather.find((item)=>{
+  if(item.city_name === searchQuery){
+    // console.log("hi")
+    return item
+    
+  }
+})
+console.log(weatherData)
 
-)
-});
+let array = weatherData.data.map(item =>{ 
+return new Forecast(`${item.weather.description}, ${item.datetime}`)
+
+})
+res.send(array)
+})
+
+class Forecast{
+  constructor(description , date){
+    this.description =description
+    this.date=date
+  
+  }
+}
+
 // let weatherInfo = DataOfWeather.weather.find((item)=>{
 //if(item.description === DataOfWeather && item.date === DataOfWeather){
 //return item
 
-class Forecast{
-constructor(description , date){
-  this.description =description
-  this.date=date
-
-}}
-
-console.log('weatherInfo', weatherInfo)
+// console.log('weatherInfo', weatherInfo)
 // console.log('dateInfo', dateInfo )
-res.send(description)
-res.send(date)
+// res.send(description)
+// res.send(date)
+// })
+
+server.get('*',(req,res)=>{
+  res.status(404).send('route is not found')
 })
+
+
 
 server.listen(PORT, () => {
   console.log(`listeneing on PORT ${PORT}`);
