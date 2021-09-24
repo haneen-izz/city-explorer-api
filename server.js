e19f102c2bc27a3e9d3024b6c58490acf9f74769
haneen <haneenjaradeh1@gmail.com>
2021-09-24 20:02:04 +0300

    lab08

--- server.js
+++ server.js
@@ -1,42 +1,58 @@
 'use strict';
-
+const axios = require('axios');
 const express = require('express');
 require('dotenv').config();
 const cors = require('cors');
 const server = express();
 const PORT = process.env.PORT;
 server.use(cors());
-const getWeather = require('./assets/weather.json')
 
+server.get('/', homeRouteHandler);
+server.get('/getWeather', getWeatherHandler);
+server.get('/getMovie', getMovieHandler);
+server.get('*', notFoundHandler);
 
-server.get('/', (req, res) => {
-
-  res.status(200).send('home route');
-});
-server.get('/test', (request, response) => {
-  response.send('api server is working');
-});
-
-server.get('/getWeather', (req, res) => {
+function homeRouteHandler(req, res) {
+  res.send('home route')
+}
+function getWeatherHandler(req, res){
 // res.send(weatherData)
-let searchQuery= req.query.searchQuery;
+let searchQuery= req.query.city;
+let weatherURL = `https://api.weatherbit.io/v2.0/forecast/daily?city=${searchQuery},&key=${process.env.WEATHER_KEY}&days=4`;
 console.log(req.query);
+console.log(weatherURL)
 // console.log(req.query.DataOfWeather)
-let weatherData = getWeather.find((item)=>{
-  if(item.city_name === searchQuery){
-    // console.log("hi")
-    return item
-    
-  }
-})
-console.log(weatherData)
 
-let array = weatherData.data.map(item =>{ 
+axios.get(weatherURL).then(weatherData => {
+  console.log(weatherData)
+  console.log(weatherData.data)
+let array =  weatherData.data.data.map(item =>{ 
 return new Forecast(`${item.weather.description}, ${item.datetime}`)
 
 })
 res.send(array)
-})
+}).catch(error => { res.send(error)
+});
+}
+function getMovieHandler(req, res){
+  // res.send(weatherData)
+  let searchQuery= req.query.searchQuery;
+  let movieURL = `https://api.themoviedb.org/3/search/movie?api_key=${process.env.MOVIE_KEY}&language=en-US&query=${searchQuery}&page=1&include_adult=false`
+  console.log(req.query);
+  console.log(movieURL)
+
+  axios.get(movieURL).then(movieData => {
+    console.log(movieData)
+    console.log(movieData.results)
+  let Moviearray =  movieData.data.results.map(item =>{ 
+  return new Movie(item.title,item.poster_path, item.overview)
+  
+  })
+  res.send(Moviearray)
+  }).catch(error => { res.send(error)
+  });
+  }
+
 
 class Forecast{
   constructor(description , date){
@@ -44,6 +60,15 @@ class Forecast{
     this.date=date
   
   }
+}
+class Movie{
+  constructor(title ,poster_path,overview){
+    this.title = title
+    this.poster_path= 'https://image.tmdb.org/t/p/w500' + poster_path
+    this.overview = overview
+  
+
+}
 }
 
 // let weatherInfo = DataOfWeather.weather.find((item)=>{
@@ -55,15 +80,13 @@ class Forecast{
 // res.send(description)
 // res.send(date)
 // })
-
-server.get('*',(req,res)=>{
+function notFoundHandler(req, res) {
   res.status(404).send('route is not found')
-})
+}
 
 
 
 server.listen(PORT, () => {
   console.log(`listeneing on PORT ${PORT}`);
 });
-
 
