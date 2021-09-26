894e6306528f39332e859d46651284f814d926f9
haneen <haneenjaradeh1@gmail.com>
2021-09-26 23:18:30 +0300

    lab10

--- module/movies.js
+++ module/movies.js
@@ -1,30 +1,39 @@
-const axios = require("axios");
+const axios = require('axios');
+let cache = require('./cache.js');
 function getMovieHandler(req, res) {
   let searchQuery = req.query.searchQuery;
   let movieURL = `https://api.themoviedb.org/3/search/movie?api_key=${process.env.MOVIE_KEY}&language=en-US&query=${searchQuery}&page=1&include_adult=false`;
-  console.log(req.query);
-  console.log(movieURL);
+  if (cache[searchQuery] !== undefined) {
+    console.log('the cashe contain data ');
+    console.log(cache);
+    res.send(cache[searchQuery]);
+  } else {
+    console.log('cache memory is empty hit the api');
+    console.log(req.query);
+    console.log(movieURL);
 
-  axios
-    .get(movieURL)
-    .then((movieData) => {
-      console.log(movieData);
-      console.log(movieData.results);
-      let Moviearray = movieData.data.results.map((item) => {
-        return new Movie(item.title, item.poster_path, item.overview);
+    axios
+      .get(movieURL)
+      .then((movieData) => {
+        console.log(movieData);
+        console.log(movieData.results);
+        let Moviearray = movieData.data.results.map((item) => {
+          return new Movie(item.title, item.poster_path, item.overview);
+        });
+        cache[searchQuery] = Moviearray;
+        res.send(Moviearray);
+      })
+      .catch((error) => {
+        res.send(error);
       });
-      res.send(Moviearray);
-    })
-    .catch((error) => {
-      res.send(error);
-    });
-}
+  }
 
-class Movie {
-  constructor(title, poster_path, overview) {
-    (this.title = title),
-      (this.poster_path = "https://image.tmdb.org/t/p/w500" + poster_path);
-    this.overview = overview;
+  class Movie {
+    constructor(title, poster_path, overview) {
+      (this.title = title),
+        (this.poster_path = 'https://image.tmdb.org/t/p/w500' + poster_path);
+      this.overview = overview;
+    }
   }
 }
 
--- module/weather.js
+++ module/weather.js
@@ -1,30 +1,40 @@
-const axios = require("axios");
-function getWeatherHandler(req, res){
+const axios = require('axios');
+let cache = require('./cache.js');
+function getWeatherHandler(req, res) {
   // res.send(weatherData)
-  let searchQuery= req.query.city;
+  let searchQuery = req.query.city;
   let weatherURL = `https://api.weatherbit.io/v2.0/forecast/daily?city=${searchQuery},&key=${process.env.WEATHER_KEY}&days=4`;
-  console.log(req.query);
-  console.log(weatherURL)
-  // console.log(req.query.DataOfWeather)
-  
-  axios.get(weatherURL).then(weatherData => {
-    console.log(weatherData)
-    console.log(weatherData.data)
-  let array =  weatherData.data.data.map(item =>{ 
-  return new Forecast(`${item.weather.description}, ${item.datetime}`)
-  
-  })
-  res.send(array)
-  }).catch(error => { res.send(error)
-  });
+  if (cache[searchQuery] !== undefined) {
+    console.log('the cashe contain data ');
+    console.log(cache);
+    res.send(cache[searchQuery]);
+  } else {
+    console.log('cache memory is empty hit the api');
+    console.log(req.query);
+    console.log(weatherURL);
+    // console.log(req.query.DataOfWeather)
+
+    axios
+      .get(weatherURL)
+      .then((weatherData) => {
+        console.log(weatherData);
+        console.log(weatherData.data);
+        let array = weatherData.data.data.map((item) => {
+          return new Forecast(`${item.weather.description}, ${item.datetime}`);
+        });
+        cache[searchQuery] = array;
+        res.send(array);
+      })
+      .catch((error) => {
+        res.send(error);
+      });
   }
 
-  class Forecast{
-    constructor(description , date){
-      this.description =description
-      this.date=date
-    
+  class Forecast {
+    constructor(description, date) {
+      this.description = description;
+      this.date = date;
     }
   }
-
+}
 module.exports = getWeatherHandler;
